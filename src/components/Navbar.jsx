c2be414b6c9f3ca510a0ccfae89677f4fe75bb80
bytesbybrave <bahadurali123470@gmail.com>
2026-03-21 22:36:31 +0500

    Add dayjs dependency and implement navigation links and icons in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
+import dayjs from 'dayjs'
+import { navIcons, navLinks } from '#constants'
 
 const Navbar = () => {
     return (
-        <div>
-            <img src="/images/logo.svg" alt="logo " />
-            <p className='font-bold'>Bahadur's Portfolio</p>
+        <nav>
+            <div>
+                <img src="/images/logo.svg" alt="logo " />
+                <p className='font-bold'>Bahadur's Portfolio</p>
 
-            <ul>
-                {[
-                    { id: 1, name: "Portfolio" },
-                    { id: 2, name: "Contact" },
-                    { id: 3, name: "Projects" },
-                ].map(({ id, name}) => (
-                    <li key={id}>
-                        <p>{name}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
+                <ul>
+                    {navLinks.map(({ id, name}) => (
+                        <li key={id}>
+                            <p>{name}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <nav>
+                <ul>
+                    {navIcons.map(({id, img}) => (
+                        <li key={id}>
+                            <img src={img} alt={`icon-${id}`} className='icon-hover'/>
+                        </li>
+                    ))}
+                </ul>
+                <time>{dayjs().format('ddd MMM D:mm A')}</time>
+            </nav>
+        </nav>
     )
 }
 
