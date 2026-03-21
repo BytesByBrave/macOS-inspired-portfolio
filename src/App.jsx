db62e997a15889768d4672ae76f5a110409d3314
bytesbybrave <bahadurali123470@gmail.com>
2026-03-21 13:27:34 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-2xl text-red-500 bg-green-400'>Welcome to my Macos inspired folio</h1>
+    </div>
+  )
+}
+
+export default App
